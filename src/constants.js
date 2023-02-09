8b631bc1b43267b7841f2972569c6d4e63a5e249
denoqo <xq@utexas.edu>
2023-02-09 02:55:25 -0600

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 export const SAT_API_KEY = "2B82F8-EEVKMV-T5V72B-4Y8P";
 export const STARLINK_CATEGORY = "52";
 export const NEARBY_SATELLITE = "rest/v1/satellite/above";
-export const BASE_URL = "https://main.d2mou3jlfxv6ut.amplifyapp.com/"; // the backend endpoint during deployment
+export const BASE_URL = "https://44.211.238.175"; // the backend endpoint during deployment
 export const WORLD_MAP_URL = "https://unpkg.com/world-atlas@1/world/110m.json";
 export const SATELLITE_POSITION_URL = "rest/v1/satellite/positions";
